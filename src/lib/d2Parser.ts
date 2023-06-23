5bff8d8bd4fd60f7576b129d72f2626482801e26
Zonarius <zonarius@live.at>
2023-06-23 10:54:58 +0200

    Convert to 2 spaces

--- src/lib/d2Parser.ts
+++ src/lib/d2Parser.ts
@@ -2,194 +2,197 @@ import JSON5 from 'json5';
 import { readTbl } from './tblParser';
 
 export interface D2Files {
-    global: {
-        excel: {
-            charstats: D2Charstat[];
-            itemstatcost: D2ItemStatCost[];
-            misc: D2Misc[];
-            monstats: D2Monster[];
-            properties: D2Property[];
-            runes: D2Runeword[];
-            skills: D2Skill[];
-            skilldesc: D2Skilldesc[];
-        }
-    };
-    hd: any;
-    local: any;
+	global: {
+		excel: {
+			charstats: D2Charstat[];
+			itemstatcost: D2ItemStatCost[];
+			misc: D2Misc[];
+			monstats: D2Monster[];
+			properties: D2Property[];
+			runes: D2Runeword[];
+			skills: D2Skill[];
+			skilldesc: D2Skilldesc[];
+			UniqueItems: D2UniqueItem[];
+		}
+	};
+	hd: any;
+	local: any;
 };
 
 export interface D2Runeword {
-    Name: string;
-    complete: string;
+	Name: string;
+	complete: string;
 
-    itype1: string;
-    itype2: string;
-    itype3: string;
-    itype4: string;
-    itype5: string;
-    itype6: string;
+	itype1: string;
+	itype2: string;
+	itype3: string;
+	itype4: string;
+	itype5: string;
+	itype6: string;
 
-    Rune1: string;
-    Rune2: string;
-    Rune3: string;
-    Rune4: string;
-    Rune5: string;
-    Rune6: string;
+	Rune1: string;
+	Rune2: string;
+	Rune3: string;
+	Rune4: string;
+	Rune5: string;
+	Rune6: string;
 }
 
-
+export interface D2UniqueItem {
+	index: string
+}
 
 export interface D2Misc {
-    name: string;
-    levelreq: string;
+	name: string;
+	levelreq: string;
 }
 
 export interface D2Property {
-    code: string;
-    func1: string;
-    stat1: string;
-    set1: string;
-    val1: string;
+	code: string;
+	func1: string;
+	stat1: string;
+	set1: string;
+	val1: string;
 
-    func2: string;
-    stat2: string;
-    set2: string;
-    val2: string;
+	func2: string;
+	stat2: string;
+	set2: string;
+	val2: string;
 
-    func3: string;
-    stat3: string;
-    set3: string;
-    val3: string;
+	func3: string;
+	stat3: string;
+	set3: string;
+	val3: string;
 
-    func4: string;
-    stat4: string;
-    set4: string;
-    val4: string;
+	func4: string;
+	stat4: string;
+	set4: string;
+	val4: string;
 
-    func5: string;
-    stat5: string;
-    set5: string;
-    val5: string;
+	func5: string;
+	stat5: string;
+	set5: string;
+	val5: string;
 
-    func6: string;
-    stat6: string;
-    set6: string;
-    val6: string;
+	func6: string;
+	stat6: string;
+	set6: string;
+	val6: string;
 
-    func7: string;
-    stat7: string;
-    set7: string;
-    val7: string;
+	func7: string;
+	stat7: string;
+	set7: string;
+	val7: string;
 }
 
 export interface D2ItemStatCost {
-    Stat: string;
-    "*ID": string;
-    ID: string;
-    descpriority: string;
-    descfunc: string;
-    descval: "0" | "1" | "2";
-    descstrpos: string;
-    descstrneg: string;
-    descstr2: string;
+	Stat: string;
+	"*ID": string;
+	ID: string;
+	descpriority: string;
+	descfunc: string;
+	descval: "0" | "1" | "2";
+	descstrpos: string;
+	descstrneg: string;
+	descstr2: string;
 }
 
 export interface D2Skill {
-    skill: string;
-    "*Id": string;
-    skilldesc: string;
-    charclass: string;
+	skill: string;
+	"*Id": string;
+	skilldesc: string;
+	charclass: string;
 }
 
 export interface D2Charstat {
-    class: string;
-    StrAllSkills: string;
-    StrSkillTab1: string;
-    StrSkillTab2: string;
-    StrSkillTab3: string;
+	class: string;
+	StrAllSkills: string;
+	StrSkillTab1: string;
+	StrSkillTab2: string;
+	StrSkillTab3: string;
 }
 
 export interface D2Skilldesc {
-    skilldesc: string;
-    "str name": string;
-    "str alt": string;
-    desctexta1: string;
-    desctextb1: string;
-    desctexta2: string;
-    desctextb2: string;
-    "item proc text": string;
+	skilldesc: string;
+	"str name": string;
+	"str alt": string;
+	desctexta1: string;
+	desctextb1: string;
+	desctexta2: string;
+	desctextb2: string;
+	"item proc text": string;
 }
 
 export interface D2Monster {
-    Id: string;
-    "*hcIdx": string;
-    NameStr: string;
+	Id: string;
+	"*hcIdx": string;
+	NameStr: string;
 }
 
 export interface FileLike {
-    webkitRelativePath: string;
-    text(): Promise<string>;
-    arrayBuffer(): Promise<Uint8Array>;
+	webkitRelativePath: string;
+	text(): Promise<string>;
+	arrayBuffer(): Promise<Uint8Array>;
 }
 
 export async function parseD2(files: FileLike[]): Promise<D2Files> {
-    let d2: any = {};
+	let d2: any = {};
 
-    for (const file of files) {
-        const rp = file.webkitRelativePath
-        const path = rp.substring(rp.indexOf("/") + 1).split("/");
-        await mkFile(d2, file, path);
-    }
+	for (const file of files) {
+		const rp = file.webkitRelativePath
+		const path = rp.substring(rp.indexOf("/") + 1).split("/");
+		await mkFile(d2, file, path);
+	}
 
-    return d2;
+	return d2;
 }
 
 async function mkFile(folder: any, file: FileLike, [fileName, ...rest]: string[]) {
-    const mkOrGet = (name: string) => {
-        if (!folder[name]) {
-            folder[name] = {}
-        }
-        return folder[name]
-    }
+	const mkOrGet = (name: string) => {
+		if (!folder[name]) {
+			folder[name] = {}
+		}
+		return folder[name]
+	}
 
-    if (rest.length >= 2) {
-        await mkFile(mkOrGet(fileName), file, rest);
-    } else if (fileName === "excel") {
-        mkOrGet(fileName)[stripExt(rest[0])] = await parseExcel(file)
-    } else if (rest[0].endsWith(".json")){
-        mkOrGet(fileName)[stripExt(rest[0])] = await parseJson(file)
-    } else if (rest[0].endsWith(".tbl")){
-        mkOrGet(fileName)[stripExt(rest[0])] = await parseTbl(file)
-    }
+	if (rest.length >= 2) {
+		await mkFile(mkOrGet(fileName), file, rest);
+	} else if (fileName === "excel") {
+		mkOrGet(fileName)[stripExt(rest[0])] = await parseExcel(file)
+	} else if (rest[0].endsWith(".json")){
+		mkOrGet(fileName)[stripExt(rest[0])] = await parseJson(file)
+	} else if (rest[0].endsWith(".tbl")){
+		mkOrGet(fileName)[stripExt(rest[0])] = await parseTbl(file)
+	}
 }
 
 function stripExt(fileName: string) {
-    return fileName.substring(0, fileName.lastIndexOf("."));
+	return fileName.substring(0, fileName.lastIndexOf("."));
 }
 
 async function parseJson(file: FileLike) {
-    const text = await file.text();
-    return JSON5.parse(text);
+	const text = await file.text();
+	return JSON5.parse(text);
 }
 
 async function parseExcel(file: FileLike) {
-    const text = await file.text();
-    const [headerLine, ...entries] = text.split("\n").filter(line => line.length > 0);
-    const header = headerLine.split("\t");
+	const text = await file.text();
+	const [headerLine, ...entries] = text.split("\n").filter(line => line.length > 0);
+	const header = headerLine.split("\t");
 
-    return entries.map(entry => {
-        const cells = entry.split("\t");
-        let entryObj: any = {};
-        for (let i = 0; i < cells.length; i++) {
-            if (cells[i] !== "") {
-                entryObj[header[i]] = cells[i];
-            }
-        }
-        return entryObj;
-    })
+	return entries.map(entry => {
+		const cells = entry.split("\t");
+		let entryObj: any = {};
+		for (let i = 0; i < cells.length; i++) {
+			if (cells[i] !== "") {
+				entryObj[header[i]] = cells[i];
+			}
+		}
+		return entryObj;
+	})
 }
 
 async function parseTbl(file: FileLike) {
-    const bin = await file.arrayBuffer();
-    return readTbl(bin);
+	const bin = await file.arrayBuffer();
+	return readTbl(bin);
 }
