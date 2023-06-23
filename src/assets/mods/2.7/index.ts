84ab9ced22ca3e1cf2478b95c1f0ed2cdb36a435
Zonarius <zonarius@live.at>
2023-06-23 16:54:13 +0200

    Convert all asset files and folders to lowercase again

--- src/assets/mods/2.7/index.ts
+++ src/assets/mods/2.7/index.ts
@@ -1,43 +1,43 @@
 import file0 from './global/excel/properties.txt?raw';
-import file1 from './global/excel/itemstatcost.txt?raw';
-import file2 from './global/excel/skills.txt?raw';
-import file3 from './global/excel/monstats.txt?raw';
-import file4 from './global/excel/skilldesc.txt?raw';
-import file5 from './global/excel/misc.txt?raw';
-import file6 from './global/excel/weapons.txt?raw';
-import file7 from './global/excel/runes.txt?raw';
+import file1 from './local/lng/eng/string.tbl?bin';
+import file2 from './local/lng/strings/item-runes.json?raw';
+import file3 from './global/excel/itemstatcost.txt?raw';
+import file4 from './global/excel/skills.txt?raw';
+import file5 from './global/excel/monstats.txt?raw';
+import file6 from './global/excel/skilldesc.txt?raw';
+import file7 from './global/excel/misc.txt?raw';
 import file8 from './global/excel/armor.txt?raw';
-import file9 from './global/excel/uniqueitems.txt?raw';
-import file10 from './global/excel/charstats.txt?raw';
-import file11 from './global/excel/setitems.txt?raw';
-import file12 from './global/excel/sets.txt?raw';
-import file13 from './local/lng/eng/expansionstring.tbl?bin';
-import file14 from './local/lng/eng/string.tbl?bin';
-import file15 from './local/lng/strings/item-runes.json?raw';
-import file16 from './local/lng/eng/patchstring.tbl?bin';
-import file17 from './local/lng/strings/monsters.json?raw';
+import file9 from './global/excel/runes.txt?raw';
+import file10 from './global/excel/weapons.txt?raw';
+import file11 from './local/lng/eng/expansionstring.tbl?bin';
+import file12 from './global/excel/charstats.txt?raw';
+import file13 from './global/excel/sets.txt?raw';
+import file14 from './global/excel/uniqueitems.txt?raw';
+import file15 from './global/excel/setitems.txt?raw';
+import file16 from './local/lng/strings/monsters.json?raw';
+import file17 from './local/lng/eng/patchstring.tbl?bin';
 import file18 from './local/lng/strings/item-modifiers.json?raw';
 import file19 from './local/lng/strings/skills.json?raw';
 
 export default {
   "global/excel/properties.txt": file0,
-  "global/excel/itemstatcost.txt": file1,
-  "global/excel/skills.txt": file2,
-  "global/excel/monstats.txt": file3,
-  "global/excel/skilldesc.txt": file4,
-  "global/excel/misc.txt": file5,
-  "global/excel/weapons.txt": file6,
-  "global/excel/runes.txt": file7,
+  "local/lng/eng/string.tbl": file1,
+  "local/lng/strings/item-runes.json": file2,
+  "global/excel/itemstatcost.txt": file3,
+  "global/excel/skills.txt": file4,
+  "global/excel/monstats.txt": file5,
+  "global/excel/skilldesc.txt": file6,
+  "global/excel/misc.txt": file7,
   "global/excel/armor.txt": file8,
-  "global/excel/uniqueitems.txt": file9,
-  "global/excel/charstats.txt": file10,
-  "global/excel/setitems.txt": file11,
-  "global/excel/sets.txt": file12,
-  "local/lng/eng/expansionstring.tbl": file13,
-  "local/lng/eng/string.tbl": file14,
-  "local/lng/strings/item-runes.json": file15,
-  "local/lng/eng/patchstring.tbl": file16,
-  "local/lng/strings/monsters.json": file17,
+  "global/excel/runes.txt": file9,
+  "global/excel/weapons.txt": file10,
+  "local/lng/eng/expansionstring.tbl": file11,
+  "global/excel/charstats.txt": file12,
+  "global/excel/sets.txt": file13,
+  "global/excel/uniqueitems.txt": file14,
+  "global/excel/setitems.txt": file15,
+  "local/lng/strings/monsters.json": file16,
+  "local/lng/eng/patchstring.tbl": file17,
   "local/lng/strings/item-modifiers.json": file18,
   "local/lng/strings/skills.json": file19,
 }
--- src/assets/mods/projectd2/index.ts
+++ src/assets/mods/projectd2/index.ts
@@ -1,35 +1,35 @@
 import file0 from './global/excel/itemstatcost.txt?raw';
-import file1 from './global/excel/properties.txt?raw';
-import file2 from './global/excel/skills.txt?raw';
-import file3 from './global/excel/monstats.txt?raw';
-import file4 from './global/excel/misc.txt?raw';
-import file5 from './global/excel/skilldesc.txt?raw';
-import file6 from './global/excel/runes.txt?raw';
-import file7 from './global/excel/weapons.txt?raw';
+import file1 from './local/lng/eng/string.tbl?bin';
+import file2 from './global/excel/properties.txt?raw';
+import file3 from './global/excel/skilldesc.txt?raw';
+import file4 from './global/excel/skills.txt?raw';
+import file5 from './global/excel/monstats.txt?raw';
+import file6 from './global/excel/weapons.txt?raw';
+import file7 from './global/excel/runes.txt?raw';
 import file8 from './global/excel/armor.txt?raw';
-import file9 from './global/excel/charstats.txt?raw';
-import file10 from './global/excel/uniqueitems.txt?raw';
-import file11 from './global/excel/setitems.txt?raw';
-import file12 from './global/excel/sets.txt?raw';
-import file13 from './local/lng/eng/string.tbl?bin';
-import file14 from './local/lng/eng/expansionstring.tbl?bin';
-import file15 from './local/lng/eng/patchstring.tbl?bin';
+import file9 from './global/excel/misc.txt?raw';
+import file10 from './local/lng/eng/patchstring.tbl?bin';
+import file11 from './global/excel/charstats.txt?raw';
+import file12 from './global/excel/uniqueitems.txt?raw';
+import file13 from './global/excel/setitems.txt?raw';
+import file14 from './global/excel/sets.txt?raw';
+import file15 from './local/lng/eng/expansionstring.tbl?bin';
 
 export default {
   "global/excel/itemstatcost.txt": file0,
-  "global/excel/properties.txt": file1,
-  "global/excel/skills.txt": file2,
-  "global/excel/monstats.txt": file3,
-  "global/excel/misc.txt": file4,
-  "global/excel/skilldesc.txt": file5,
-  "global/excel/runes.txt": file6,
-  "global/excel/weapons.txt": file7,
+  "local/lng/eng/string.tbl": file1,
+  "global/excel/properties.txt": file2,
+  "global/excel/skilldesc.txt": file3,
+  "global/excel/skills.txt": file4,
+  "global/excel/monstats.txt": file5,
+  "global/excel/weapons.txt": file6,
+  "global/excel/runes.txt": file7,
   "global/excel/armor.txt": file8,
-  "global/excel/charstats.txt": file9,
-  "global/excel/uniqueitems.txt": file10,
-  "global/excel/setitems.txt": file11,
-  "global/excel/sets.txt": file12,
-  "local/lng/eng/string.tbl": file13,
-  "local/lng/eng/expansionstring.tbl": file14,
-  "local/lng/eng/patchstring.tbl": file15,
+  "global/excel/misc.txt": file9,
+  "local/lng/eng/patchstring.tbl": file10,
+  "global/excel/charstats.txt": file11,
+  "global/excel/uniqueitems.txt": file12,
+  "global/excel/setitems.txt": file13,
+  "global/excel/sets.txt": file14,
+  "local/lng/eng/expansionstring.tbl": file15,
 }
