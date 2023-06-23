b268bf0e89872823c1129d61689eaff06f209710
Zonarius <zonarius@live.at>
2023-06-23 16:56:18 +0200

    Recover mod files, now lower case hopefully

---
+++ src/assets/mods/2.7/index.ts
@@ -0,0 +1,43 @@
+import file0 from './global/excel/itemstatcost.txt?raw';
+import file1 from './local/lng/eng/string.tbl?bin';
+import file2 from './local/lng/strings/item-modifiers.json?raw';
+import file3 from './global/excel/skills.txt?raw';
+import file4 from './global/excel/properties.txt?raw';
+import file5 from './global/excel/monstats.txt?raw';
+import file6 from './global/excel/weapons.txt?raw';
+import file7 from './global/excel/skilldesc.txt?raw';
+import file8 from './global/excel/charstats.txt?raw';
+import file9 from './global/excel/misc.txt?raw';
+import file10 from './global/excel/runes.txt?raw';
+import file11 from './global/excel/setitems.txt?raw';
+import file12 from './global/excel/armor.txt?raw';
+import file13 from './global/excel/uniqueitems.txt?raw';
+import file14 from './global/excel/sets.txt?raw';
+import file15 from './local/lng/eng/patchstring.tbl?bin';
+import file16 from './local/lng/strings/item-runes.json?raw';
+import file17 from './local/lng/eng/expansionstring.tbl?bin';
+import file18 from './local/lng/strings/monsters.json?raw';
+import file19 from './local/lng/strings/skills.json?raw';
+
+export default {
+  "global/excel/itemstatcost.txt": file0,
+  "local/lng/eng/string.tbl": file1,
+  "local/lng/strings/item-modifiers.json": file2,
+  "global/excel/skills.txt": file3,
+  "global/excel/properties.txt": file4,
+  "global/excel/monstats.txt": file5,
+  "global/excel/weapons.txt": file6,
+  "global/excel/skilldesc.txt": file7,
+  "global/excel/charstats.txt": file8,
+  "global/excel/misc.txt": file9,
+  "global/excel/runes.txt": file10,
+  "global/excel/setitems.txt": file11,
+  "global/excel/armor.txt": file12,
+  "global/excel/uniqueitems.txt": file13,
+  "global/excel/sets.txt": file14,
+  "local/lng/eng/patchstring.tbl": file15,
+  "local/lng/strings/item-runes.json": file16,
+  "local/lng/eng/expansionstring.tbl": file17,
+  "local/lng/strings/monsters.json": file18,
+  "local/lng/strings/skills.json": file19,
+}
