2d8da014aa138d4918bb01bc007cce2b76dd645b
Zonarius <zonarius@live.at>
2023-07-05 06:53:52 +0200

    Improve search result link

--- src/routes/mod/search.tsx
+++ src/routes/mod/search.tsx
@@ -1,8 +1,8 @@
 import Input from "@mui/joy/Input";
 import { useSearchIndex } from "../../lib/searchIndex";
-import { List, ListItem, ListItemButton } from "@mui/joy";
+import { Link, List, ListItem, ListItemButton } from "@mui/joy";
 import { SearchResult } from "minisearch";
-import { Link, useParams } from "react-router-dom";
+import { Link as RouterLink, useParams } from "react-router-dom";
 import { useUrlState } from "../../lib/hooks";
 import { ExcelFileName } from "../../context/referenceBuilder";
 
@@ -22,8 +22,10 @@ function Search() {
       <List>
         {searchResults.map(result => (
           <ListItem key={result.id}>
-            <ListItemButton component={Link} to={`/${mod}/${result.file}/${result.id}`}>
-              <span className={classByFile(result.file)}>{result.text}</span>
+            <ListItemButton>
+              <Link component={RouterLink} to={`/${mod}/${result.file}/${result.id}`} overlay>
+                <span className={classByFile(result.file)}>{result.text}</span>
+              </Link>
             </ListItemButton>
           </ListItem>
         ))}
@@ -31,7 +33,6 @@ function Search() {
     </>
   );
 }
-
 
 function classByFile(file: ExcelFileName) {
   switch (file) {
